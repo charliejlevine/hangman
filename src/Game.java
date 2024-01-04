b6dc44deb318046a3cb4f8c8785041f45104305e
charlielevine <charliechamp616@gmail.com>
2024-01-04 02:44:51 -0800

    enhancements

--- src/Game.java
+++ src/Game.java
@@ -9,33 +9,95 @@ public class Game {
 
     private int numMistakes = 0;
 
-    private int gameState = -1; //-1: in-progress, 0: loss, 1: win
+    private String[] hangman = {"  +---+", "      |", "      |", "      |", "      |", "      |", "========="};
 
-    public Game(String word) {
+    private int gameState; // 0: in-progress, 1: loss, 2: win
+
+    private int difficulty; // 0: easy, 1: medium: 2: hard
+
+    private int category; // 0: animals, 1: fruits, 2: countries
+
+    public Game(int difficulty, int category, String word) {
+        this.difficulty = difficulty;
+        this.category = category;
         this.word = word;
 
         for (char letter : word.toCharArray()) {
             wordProgress.append("_");
         }
 
+        System.out.println("Difficulty: " + GameManager.DIFFICULTIES[difficulty]);
+        System.out.println("Category: " + GameManager.CATEGORIES[category]);
+        updateHangman(numMistakes);
         System.out.println(wordProgress);
     }
 
+    public String getDifficulty() {
+        return GameManager.DIFFICULTIES[difficulty];
+    }
+
+    public String getCategory() {
+        return GameManager.CATEGORIES[category];
+    }
+
+    public int getNumMistakes() {
+        return numMistakes;
+    }
+
+    public int getGameState() {
+        return gameState;
+    }
+
+    private void updateHangman(int numMistakes) {
+        switch (numMistakes) {
+            case 1:
+                hangman[1] = "  |   |";
+                break;
+            case 2:
+                hangman[2] = "  O   |";
+                break;
+            case 3:
+                hangman[3] = "  |   |";
+                break;
+            case 4:
+                hangman[3] = " /|   |";
+                break;
+            case 5:
+                hangman[3] = " /|\\  |";
+                break;
+            case 6:
+                hangman[4] = " /    |";
+                break;
+            case 7:
+                hangman[4] = " / \\  |";
+                break;
+        }
+
+        for(String part : hangman) {
+            System.out.println(part);
+        }
+    }
+
     public void createGuess(String input) {
-        if (gameState != -1) {
-            System.out.println("Game over... Type NEW to play again!");
+        if (gameState != 0) {
+            System.out.println("Game over... Reply NEW to play again or SCORE for high scores!");
             return;
         }
 
         Guess guess = new Guess(input, guesses, word);
         guesses.add(guess);
 
-        if (guess.isValid() && guess.getCorrectIndexes().isEmpty()) {
-            System.out.println(++numMistakes + "/7 mistakes!");
-            if (numMistakes == 7) {
-                System.out.println("You lose... Type NEW to play again!");
-                gameState = 0;
-                return;
+        if (guess.isValid()) {
+            if (guess.getCorrectIndexes().isEmpty()) {
+                System.out.println("Incorrect! " + ++numMistakes + "/7 mistakes.");
+                updateHangman(numMistakes);
+                if (numMistakes == 7) {
+                    System.out.println("You lose... Reply NEW to play again or SCORE for high scores!");
+                    gameState = 1;
+                    return;
+                }
+            } else {
+                System.out.println("Correct!");
             }
         }
 
@@ -46,8 +108,8 @@ public class Game {
         System.out.println(wordProgress);
 
         if (wordProgress.toString().equals(word)) {
-            System.out.println("You win! Type NEW to play again!");
-            gameState = 1;
+            System.out.println("You win! Reply NEW to play again or SCORE for high scores!");
+            gameState = 2;
         }
     }
 }
--- src/GameManager.java
+++ src/GameManager.java
@@ -1,13 +1,31 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class GameManager {
     private static final Random random = new Random();
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    private final String[] words = {"apple", "orange", "banana"};
+    public static final String[] DIFFICULTIES = {"Easy", "Medium", "Hard"};
+
+    public static final String[] CATEGORIES = {"Animals", "Fruits", "Countries"};
+
+    public static final String[][][] WORDS = {
+            {
+                    {"dog", "cat", "bird"},
+                    {"apple", "banana", "orange"},
+                    {"usa", "canada", "mexico"}
+            },
+            {
+                    {"elephant", "giraffe", "lion"},
+                    {"strawberry", "grapefruit", "kiwi"},
+                    {"brazil", "china", "india"}
+            },
+            {
+                    {"hippopotamus", "rhinoceros", "crocodile"},
+                    {"pomegranate", "pineapple", "mango"},
+                    {"australia", "singapore", "venezuela"}
+            }
+    };
 
     private final ArrayList<Game> games = new ArrayList<Game>();
 
@@ -24,15 +42,64 @@ public class GameManager {
                 continue;
             }
 
+            if (input.equalsIgnoreCase("score")) {
+                games.sort(Comparator.comparingInt(Game::getNumMistakes));
+                System.out.printf("%-15s %-15s %-15s%n", "Difficulty", "Category", "Mistakes");
+                System.out.println("-----------------------------------------------");
+                for (Game game : games) {
+                    if (game.getGameState() == 2) {
+                        System.out.printf("%-15s %-15s %-15d%n", game.getDifficulty(), game.getCategory(), game.getNumMistakes());
+                    }
+                }
+                continue;
+            }
+
             game.createGuess(input);
         }
     }
 
     public void createGame() {
+        System.out.println("---New Hangman Game---");
+
+        int difficulty = 0;
+        while (difficulty == 0) {
+            System.out.println("Reply EASY, MEDIUM, or HARD to choose your difficulty!");
+            String input = scanner.nextLine();
+            switch (input.toLowerCase()) {
+                case "easy":
+                    difficulty = 1;
+                    break;
+                case "medium":
+                    difficulty = 2;
+                    break;
+                case "hard":
+                    difficulty = 3;
+                    break;
+            }
+        }
+
+        int category = 0;
+        while (category == 0) {
+            System.out.println("Reply ANIMALS, FRUITS, or COUNTRIES to choose your category!");
+            String input = scanner.nextLine();
+            switch (input.toLowerCase()) {
+                case "animals":
+                    category = 1;
+                    break;
+                case "fruits":
+                    category = 2;
+                    break;
+                case "countries":
+                    category = 3;
+                    break;
+            }
+        }
+
+        String[] words = WORDS[difficulty - 1][category - 1];
         int numWords = words.length;
         int randIndex = random.nextInt(numWords);
         String word = words[randIndex];
-        game = new Game(word);
+        game = new Game(difficulty - 1, category - 1, word);
         games.add(game);
     }
 }
